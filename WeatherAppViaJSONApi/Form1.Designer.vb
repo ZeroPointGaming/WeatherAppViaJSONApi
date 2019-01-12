41f278285dda863e2f19a826b411df0c9a99aa36
LordComfy <comfyseven4@gmail.com>
2019-01-12 11:12:31 -0500

    Add project files.

---
+++ WeatherAppViaJSONApi/Form1.Designer.vb
@@ -0,0 +1,225 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Weather: "
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label2.Location = New System.Drawing.Point(51, 347)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(110, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "GEOGRAPHIC DATA"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.ListBox1)
+        Me.Panel2.Location = New System.Drawing.Point(278, 24)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(183, 467)
+        Me.Panel2.TabIndex = 3
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.ListBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.ListBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.HorizontalScrollbar = True
+        Me.ListBox1.IntegralHeight = False
+        Me.ListBox1.Location = New System.Drawing.Point(0, 0)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(181, 465)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label3.Location = New System.Drawing.Point(311, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(123, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "RAW WEATHER DATA"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel3.Controls.Add(Me.ListBox2)
+        Me.Panel3.Location = New System.Drawing.Point(12, 363)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(200, 129)
+        Me.Panel3.TabIndex = 5
+        '
+        'ListBox2
+        '
+        Me.ListBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.ListBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.ListBox2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListBox2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.HorizontalScrollbar = True
+        Me.ListBox2.IntegralHeight = False
+        Me.ListBox2.Location = New System.Drawing.Point(0, 0)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(198, 127)
+        Me.ListBox2.TabIndex = 0
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label4.Location = New System.Drawing.Point(12, 28)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(94, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Geographic Data: "
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label5.Location = New System.Drawing.Point(12, 50)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(73, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Tempurature: "
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label6.Location = New System.Drawing.Point(12, 72)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(117, 13)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Tempurature Forecast: "
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 133)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(200, 200)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        Me.ToolTip1.SetToolTip(Me.PictureBox1, "Use this to find the wind direction using the wind heading degree value in the ra" &
+        "w weather data list.")
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label7.Location = New System.Drawing.Point(51, 117)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(106, 13)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "COMPASS LEGEND"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(25, Byte), Integer), CType(CType(25, Byte), Integer), CType(CType(25, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(473, 505)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(489, 544)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(489, 544)
+        Me.Name = "Form1"
+        Me.Text = "Weather App"
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel3.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents ToolTip1 As ToolTip
+End Class
---
+++ WeatherAppViaJSONApi/Form1.vb
@@ -0,0 +1,224 @@
+﻿Imports System.Web.Script.Serialization
+Imports System.IO
+Imports System.Net
+
+Public Class Form1
+    'Location Data
+    Public lat As String = My.Settings.Latitude
+    Public log As String = My.Settings.Longitude
+
+    'Retrieve location data via ip address
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        RefreshLocationData()
+
+        If My.Settings.Latitude = Nothing And My.Settings.Longitude = Nothing Then
+            Try
+                'Query location data
+                Dim Req As HttpWebRequest
+                Dim Ret As HttpWebResponse = Nothing
+                Dim SR As StreamReader
+                Req = DirectCast(WebRequest.Create("http://ip-api.com/json"), HttpWebRequest)
+                Ret = DirectCast(Req.GetResponse(), HttpWebResponse)
+                SR = New StreamReader(Ret.GetResponseStream())
+                Dim Raw As String = Nothing
+                Raw = SR.ReadToEnd()
+                Dim JavaScriptSerialization As New JavaScriptSerializer()
+                Dim ipdata_object As New IPData()
+
+                ipdata_object = JavaScriptSerialization.Deserialize(Raw, ipdata_object.GetType)
+
+                lat = ipdata_object.lat
+                log = ipdata_object.lon
+                My.Settings.Latitude = lat
+                My.Settings.Longitude = log
+                My.Settings.Save()
+            Catch ex As Exception
+                MessageBox.Show(ex.ToString())
+            End Try
+        Else
+            lat = My.Settings.Latitude
+            log = My.Settings.Longitude
+        End If
+
+        CollectWeatherData(lat, log)
+    End Sub
+
+    'generate your own free api key at openweathermap.org to use with your own project.
+    Public api_key As String = "&APPID=a756bb2cdd548defd8539e8e36c1d2df"
+    Public api_measurement As String = "imperial"
+    'Public weather_url As String = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + log + "&APPID=" + api_key + "&units=" + api_measurement
+    'Public weather_url As String = "http://api.openweathermap.org/data/2.5/weather?lat=35.6817&lon=-80.5615&APPID=a756bb2cdd548defd8539e8e36c1d2df&units=imperial"
+    Public weather_url As String = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + log + "&APPID=a756bb2cdd548defd8539e8e36c1d2df&units=imperial"
+
+    'Refresh Location Data
+    Public Sub RefreshLocationData()
+        ListBox2.Items.Clear()
+
+        'Query location data
+        Dim Req As HttpWebRequest
+        Dim Ret As HttpWebResponse = Nothing
+        Dim SR As StreamReader
+        Req = DirectCast(WebRequest.Create("http://ip-api.com/json"), HttpWebRequest)
+        Ret = DirectCast(Req.GetResponse(), HttpWebResponse)
+        SR = New StreamReader(Ret.GetResponseStream())
+        Dim Raw As String = Nothing
+        Raw = SR.ReadToEnd()
+        Dim JavaScriptSerialization As New JavaScriptSerializer()
+        Dim ipdata_object As New IPData()
+
+        ipdata_object = JavaScriptSerialization.Deserialize(Raw, ipdata_object.GetType)
+
+        ListBox2.Items.Add("----- Coordinates -----")
+        ListBox2.Items.Add("Latitude: " + ipdata_object.lat)
+        ListBox2.Items.Add("Longitude: " + ipdata_object.lon)
+        ListBox2.Items.Add("")
+
+        ListBox2.Items.Add("----- Location -----")
+        ListBox2.Items.Add("Country: " + ipdata_object.country)
+        ListBox2.Items.Add("City: " + ipdata_object.city)
+        ListBox2.Items.Add("Zip Code: " + ipdata_object.zip)
+        ListBox2.Items.Add("")
+
+        ListBox2.Items.Add("----- User Data -----")
+        ListBox2.Items.Add("ISP: " + ipdata_object.isp)
+        ListBox2.Items.Add("ORG: " + ipdata_object.org)
+        ListBox2.Items.Add("Timezone: " + ipdata_object.timezone)
+        ListBox2.Items.Add("IP: " + ipdata_object.query)
+        ListBox2.Items.Add("")
+
+        ListBox2.Items.Add("----- Region Data -----")
+        ListBox2.Items.Add("Country Code: " + ipdata_object.countryCode)
+        ListBox2.Items.Add("Region Name: " + ipdata_object.regionName)
+        ListBox2.Items.Add("Region: " + ipdata_object.region)
+    End Sub
+
+    'collect data 
+    Public Sub CollectWeatherData(latitude As String, longitude As String)
+        Try
+            'Reset application
+            ListBox1.Items.Clear()
+
+            'Query weather data
+            Dim Req As HttpWebRequest
+            Dim Ret As HttpWebResponse = Nothing
+            Dim SR As StreamReader
+            Req = DirectCast(WebRequest.Create(weather_url), HttpWebRequest)
+            Ret = DirectCast(Req.GetResponse(), HttpWebResponse)
+            SR = New StreamReader(Ret.GetResponseStream())
+            Dim Raw As String = Nothing
+            Raw = SR.ReadToEnd()
+            Dim JavaScriptSerialization As New JavaScriptSerializer()
+            Dim weatherdata As New WeatherResponse()
+
+            weatherdata = JavaScriptSerialization.Deserialize(Raw, weatherdata.GetType)
+
+            'populate the labels with data
+            Label1.Text = "Weather: " + weatherdata.weather(0).main
+            Label4.Text = "Geographic Coordinates: " + lat + " , " + log
+            Label5.Text = "Tempurature: " + weatherdata.main.temp.ToString() + "°"
+            Label6.Text = "Tempurature Forecast: " + "High " + weatherdata.main.temp_max.ToString() + "°" + " / " + "Low " + weatherdata.main.temp_min.ToString() + "°"
+
+            'set application icon
+            Me.Icon = DirectCast(My.Resources.ResourceManager.GetObject("_" + weatherdata.weather(0).icon.ToString()), Icon)
+
+            'Populate raw weather data
+            ListBox1.Items.Add("----- Weather Data -----")
+            ListBox1.Items.Add("Forecast: " + weatherdata.weather(0).main)
+            ListBox1.Items.Add("Tempurature: " + weatherdata.main.temp + "° F")
+            ListBox1.Items.Add("Wind Speed: " + weatherdata.wind.speed + " mph")
+            ListBox1.Items.Add("Wind Direction: " + weatherdata.wind.deg + "°")
+            ListBox1.Items.Add("")
+
+            ListBox1.Items.Add("----- Systemic Data -----")
+            ListBox1.Items.Add("Weather ID: " + weatherdata.weather(0).id)
+            ListBox1.Items.Add("Weather Icon: " + weatherdata.weather(0).icon)
+            ListBox1.Items.Add("Country Code: " + weatherdata.sys.country)
+            ListBox1.Items.Add("Sunrise Data:  " + weatherdata.sys.sunrise)
+            ListBox1.Items.Add("Sunset Data: " + weatherdata.sys.sunset)
+            ListBox1.Items.Add("Response ID: " + weatherdata.id)
+
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString())
+        End Try
+    End Sub
+End Class
+
+'IP Data API Deserialization classes for http://ip-api.com/json API response
+<Serializable>
+Class IPData
+    Public asinfo As String
+    Public city As String
+    Public country As String
+    Public countryCode As String
+    Public isp As String
+    Public lat As String
+    Public lon As String
+    Public org As String
+    Public query As String
+    Public region As String
+    Public regionName As String
+    Public status As String
+    Public timezone As String
+    Public zip As String
+End Class
+
+'Weather API Deserialization classes for openweathermap.org API response
+<Serializable>
+Class WeatherResponse
+    Public coord As CoordinateInfo
+    Public weather As WeatherInfo()
+    Public base As String
+    Public main As MainInfo
+    Public visibility As String
+    Public wind As WindInfo
+    Public clouds As CloudInfo
+    Public dt As String
+    Public sys As SysInfo
+    Public id As String
+    Public name As String 'Location Name
+    Public cod As String
+End Class
+
+<SerializableAttribute>
+Class CoordinateInfo
+    Public lon As String
+    Public lat As String
+End Class
+
+<SerializableAttribute>
+Class WeatherInfo
+    Public id As String
+    Public main As String 'Weather Type
+    Public description As String 'Weather Description
+    Public icon As String
+End Class
+
+<SerializableAttribute>
+Class MainInfo
+    Public temp As String
+    Public pressure As String
+    Public humidity As String
+    Public temp_min As String
+    Public temp_max As String
+End Class
+
+<SerializableAttribute>
+Class WindInfo
+    Public speed As String 'Wind Speed
+    Public deg As String 'Wind Heading
+End Class
+
+<SerializableAttribute>
+Class CloudInfo
+    Public all As String
+End Class
+
+<SerializableAttribute>
+Class SysInfo
+    Public type As String
+    Public id As String
+    Public message As String
+    Public country As String
+    Public sunrise As String
+    Public sunset As String
+End Class
